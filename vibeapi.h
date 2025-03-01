2f0d7eaf26a2a09db61359980e3dd6a3500933ed
[-=KILL MAN=-] <mctesting1@yandex.ru>
2025-03-01 12:50:47 +0300

    Create vibeapi.h

---
+++ vibeapi.h
@@ -0,0 +1,25 @@
+#ifndef VIBE_API_H
+#define VIBE_API_H
+
+
+
+#include <stdint.h>
+
+// Types
+typedef uint8_t VibeUInt8;
+typedef int16_t VibeInt16;
+typedef int32_t VibeInt32;
+typedef int64_t VibeInt64;
+typedef VibeInt32 VibeStatus;
+
+// Macros
+#define BUILD_DATE_STR __DATE__
+#define VERSION_MAJOR ( 5 )
+#define VERSION_MINOR ( 0 )
+#define VERSION_BUILD ( 0 )
+#define VERSION_REVIS ( 11 )
+#define VERSION_INT   ( VERSION_REVIS | (VERSION_BUILD << 8) | (VERSION_MINOR << 16) | (VERSION_MAJOR << 24) )
+
+
+
+#endif // VIBE_API_H
