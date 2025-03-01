3620a89c6312b334ce25951074e512a4eb0deb82
[-=KILL MAN=-] <mctesting1@yandex.ru>
2025-03-01 15:30:45 +0300

    Update immvibe.cpp

--- immvibe.cpp
+++ immvibe.cpp
@@ -1,23 +1,61 @@
 #include "vibeapi.h"
 
+extern "C"
+{
+
 jobject g_Activity = NULL;
 jobject g_ContextObj = NULL;
-bool g_Emulator = false;
+bool g_bEmulator = false;
 JavaVM* g_JavaVM = NULL;
 VibeInt32 g_nTSPVersion = 0;
 void* g_pBestEffectSet = NULL; // unused
 
-extern "C" VibeStatus ImmVibeInitialize2(int unused, JavaVM* vm, jobject activity)
+jobject g_VibratorObject;
+jmethodID g_VibrateMethod;
+
+inline void* ImmVibeInitThread(void* data)
 {
-    return 0;
+    JNIEnv* env;
+    g_JavaVM->AttachCurrentThread((void**)&env, NULL);
+    
+    jclass vibratorCls = env->FindClass("android/os/Vibrator");
+    jclass contextCls = env->FindClass("android/context/Context");
+    jmethodID sysServiceMethod = env->GetMethodID(contextCls, "getSystemService", "(Ljava/lang/String;)Ljava/lang/Object;");
+    jfieldID vibratorSrvField = env->GetStaticFieldID(contextCls, "VIBRATOR_SERVICE", "Ljava/lang/String;");
+    jstring vibratorFieldStr = env->GetStaticObjectField(contextCls, vibratorSrvField);
+    jobject localVibrateObject = env->CallObjectMethod(g_ContextObj, sysServiceMethod, vibratorSrvField);
+
+    g_VibratorObject = env->NewGlobalRef(localVibrateObject);
+    g_VibrateMethod = env->GetMethodID(vibratorCls, "vibrate", "(J)V");
+
+    // TODO:
+
+    return (void*)STATUS_OK;
 }
 
-extern "C" VibeStatus ImmVibeTerminate()
+VibeStatus ImmVibeInitialize2(int unused, JavaVM* vm, jobject activity)
 {
-    return 0;
+    g_bEmulator = false;
+    g_JavaVM = vm;
+
+    JNIEnv* env;
+    vm->AttachCurrentThread((void**)&env, NULL);
+    g_ContextObj = env->NewGlobalRef(activity);
+    if(!activity) return STATUS_FAILED_GENERIC;
+
+    // TODO:
+    
+    return STATUS_OK;
 }
 
-extern "C" VibeStatus ImmVibeTerminate2()
+VibeStatus ImmVibeTerminate()
 {
-    return 0;
+    return STATUS_OK;
 }
+
+VibeStatus ImmVibeTerminate2()
+{
+    return STATUS_OK;
+}
+
+};
